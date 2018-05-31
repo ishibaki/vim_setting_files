f3d178a6a20adb1320b10b23af5a1eaa4b6567d0
ishibaki <iroirotomoki@gmail.com>
2018-05-31 22:42:19 +0900

    Add initial_setting file

---
+++ initial_setting.sh
@@ -0,0 +1,12 @@
+DIRPATH_REPO="~/dotfiles/vim_setting_files"
+
+if [ ! -e ${DIRPATH_REPO} ]; then
+    if [ ! -e ~/dotfiles ]; then
+        makedir ~/dotfiles
+    fi
+    mv ../vim_setting_files ${DIRPATH_REPO}
+    ln -s ${DIRPATH_REPO}/.vimrc ~/.vimrc
+    ln -s ${DIRPATH_REPO}/.gvimrc ~/.gvimrc
+    ln -s ${DIRPATH_REPO}/.vim/ ~/.vim/
+fi
+

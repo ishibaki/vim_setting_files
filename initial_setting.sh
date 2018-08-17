bad7bddb4a29fe3798a7d991ec336313ded5efdd
ishibaki <iroirotomoki@gmail.com>
2018-08-17 18:42:17 +0900

    Add sentence to remove symbolic link

--- initial_setting.sh
+++ initial_setting.sh
@@ -11,6 +11,18 @@ if [ ! -e "${DIRPATH_REPO}" ]; then
 fi
 
 cp "../vim_setting_files" "${DIRPATH_REPO}"
+
+if [ -e ~/.vimrc ]; then
+    rm ~/.vimrc
+fi
 ln -s "${DIRPATH_REPO}/.vimrc" "~/.vimrc"
+
+if [ -e ~/.gvimrc ]; then
+    rm ~/.gvimrc
+fi
 ln -s "${DIRPATH_REPO}/.gvimrc" "~/.gvimrc"
+
+if [ -e ~/.vim ]; then
+    rm -rf ~/.vim
+fi
 ln -s "${DIRPATH_REPO}/.vim" "~/.vim"
